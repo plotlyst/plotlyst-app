cd86a2977fc40689dd4ed7cd01c782e5659a316c
Zsolt Kovari <kovari.zsolt91@gmail.com>
2025-03-17 09:09:58 +0100

    cp flag

--- src/main/python/plotlyst/core/domain.py
+++ src/main/python/plotlyst/core/domain.py
@@ -3800,12 +3800,6 @@ def to_node(x: float, y: float, type: GraphicsItemType, subtype: str = '', defau
     return node
 
 
-class ConnectorShape(Enum):
-    QUAD = 0
-    CURVE = 1
-    STRAIGHT = 2
-
-
 @dataclass
 class Connector:
     source_id: uuid.UUID
@@ -3822,6 +3816,7 @@ class Connector:
     cp_y: Optional[float] = None
     start_arrow_enabled: bool = field(default=False, metadata=config(exclude=exclude_if_false))
     end_arrow_enabled: bool = field(default=True, metadata=config(exclude=exclude_if_true))
+    cp_controlled: Optional[bool] = None
 
 
 @dataclass_json(undefined=Undefined.EXCLUDE)
