7b950ff26c1717d4716584d8b99b3dd08f84f610
Zsolt Kovari <kovari.zsolt91@gmail.com>
2025-06-23 15:19:54 +0200

    root side not visible

--- src/main/python/plotlyst/core/domain.py
+++ src/main/python/plotlyst/core/domain.py
@@ -2506,7 +2506,7 @@ def worldbuilding_root() -> WorldBuildingEntity:
     main_section.blocks.append(WorldBuildingEntityElement(WorldBuildingEntityElementType.Header))
     main_section.blocks.append(WorldBuildingEntityElement(WorldBuildingEntityElementType.Text))
     return WorldBuildingEntity('My world', icon='mdi.globe-model', bg_color='#40916c',
-                               elements=[main_section])
+                               elements=[main_section], side_visible=False)
 
 
 @dataclass
