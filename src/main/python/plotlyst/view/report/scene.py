15144b077bc56f3c7bfb0f32bee8c6e8a397e662
Zsolt Kovari <kovari.zsolt91@gmail.com>
2025-06-30 16:06:57 +0200

    progress

--- src/main/python/plotlyst/view/report/scene.py
+++ src/main/python/plotlyst/view/report/scene.py
@@ -1,18 +1,18 @@
 from typing import Optional, Dict
 
 from PyQt6.QtCharts import QPieSeries, QPieSlice
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt, QSize
 from PyQt6.QtGui import QCursor
-from PyQt6.QtWidgets import QToolTip
+from PyQt6.QtWidgets import QToolTip, QWidget, QPushButton, QFrame
 from overrides import overrides
-from qthandy import vbox
+from qthandy import vbox, hbox, sp, translucent
 
-from plotlyst.core.domain import Novel, Character
+from plotlyst.core.domain import Novel, Character, Tier
 from plotlyst.core.text import html
 from plotlyst.env import app_env
 from plotlyst.service.cache import acts_registry
-from plotlyst.view.common import icon_to_html_img, columns, rows, wrap
-from plotlyst.view.icons import avatars
+from plotlyst.view.common import icon_to_html_img, columns, rows, wrap, label
+from plotlyst.view.icons import avatars, IconRegistry
 from plotlyst.view.report import AbstractReport
 from plotlyst.view.widget.chart import BaseChart, ActDistributionChart
 from plotlyst.view.widget.display import ChartView, PremiumOverlayWidget
@@ -43,13 +43,14 @@ class SceneReport(AbstractReport):
         self.wdgDistributions.layout().addWidget(wrap(self.chartViewActDistribution, margin_top=35))
         self.wdgDistributions.layout().addWidget(self.wdgPov)
 
-        self.wdgConflict = rows()
+        self.conflictDisplay = ConflictSTierDisplay()
 
         self.layout().addWidget(self.wdgDistributions)
-        self.layout().addWidget(self.wdgConflict)
+        self.layout().addWidget(label("Conflict tier list", h3=True), alignment=Qt.AlignmentFlag.AlignCenter)
+        self.layout().addWidget(self.conflictDisplay)
 
         if not app_env.profile().get('scene-functions', False):
-            PremiumOverlayWidget(self.wdgConflict, 'Conflict reports',
+            PremiumOverlayWidget(self.conflictDisplay, 'Conflict reports',
                                  icon='mdi.sword-cross',
                                  alt_link='https://plotlyst.com/docs/scenes/')
 
@@ -105,3 +106,50 @@ class PovDistributionChart(BaseChart):
             QToolTip.showText(QCursor.pos(), slice.label() + " {:.1f}%".format(100 * slice.percentage()))
         else:
             QToolTip.hideText()
+
+
+class STierRow(QWidget):
+    def __init__(self, tier: Tier, parent=None):
+        super().__init__(parent)
+        self.tier = tier
+        hbox(self, 0, 0)
+
+        self.lbl = QPushButton()
+        self.lbl.setIconSize(QSize(55, 55))
+        sp(self.lbl).h_max()
+        self.lbl.setStyleSheet(f'''
+                    QPushButton {{
+                        background: {self.tier.color()};
+                        border: 1px solid lightgrey;
+                        padding: 15px;
+                        padding-top: 25px;
+                        padding-bottom: 25px;
+                        border-top-left-radius: 6px;
+                        border-bottom-left-radius: 6px;
+                    }}
+                ''')
+        if self.tier.intensity() < 4:
+            translucent(self, 0.25 * self.tier.intensity())
+
+        self.lbl.setIcon(
+            IconRegistry.from_name(f'mdi6.alpha-{self.tier.value}', scale=1.4))
+
+        self.layout().addWidget(self.lbl, alignment=Qt.AlignmentFlag.AlignLeft)
+
+
+class ConflictSTierDisplay(QFrame):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        vbox(self, 0, 0)
+        self.setProperty('muted-bg', True)
+        self.setProperty('large-rounded', True)
+
+        self.__initTierWidget(Tier.S)
+        self.__initTierWidget(Tier.A)
+        self.__initTierWidget(Tier.B)
+        self.__initTierWidget(Tier.C)
+        self.__initTierWidget(Tier.D)
+
+    def __initTierWidget(self, tier: Tier):
+        wdg = STierRow(tier)
+        self.layout().addWidget(wdg)
